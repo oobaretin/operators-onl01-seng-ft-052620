6c23805ff1f254d0df551d0f7e871b53b3887834
osagie obaretin <osagie.codes@gmail.com>
2020-06-07 05:10:08 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,20 +1,3 @@
 
 
-def unsafe?(speed)
-  if speed > 60 == speed < 40
-    return "true"
-  else
-    return "false"
-  end
-end
-
-
-
-def not_safe?(speed)
-	if speed < 40 == speed > 60 ? true : false
-
-  end
-end
-	
-
 
